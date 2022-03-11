3017c17ca095393d95c57f8ad5c853097410f977
Donnahue George <Donnahuegjr@gmail.com>
2022-03-11 09:33:47 -0500

    adding conditionals and fixed typos

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-from mimetypes import init
 from cryptography.fernet import Fernet
 
 
@@ -7,7 +6,7 @@ class PasswordManager:
     def __init__(self):
         self.key = None
         self.password_file = None
-        self.passowrd_dict = None
+        self.password_dict = {}
 
     def create_key(self, path):
         self.key = Fernet.generate_key()
@@ -31,7 +30,7 @@ class PasswordManager:
         with open(path, 'r') as file:
             for line in file:
                 site, encrypted = line.split(':')
-                self.passowrd_dict[site] = Fernet(
+                self.password_dict[site] = Fernet(
                     self.key).decrypt(encrypted.encode()).decode()
 
     def add_password(self, site, password):
@@ -42,7 +41,7 @@ class PasswordManager:
                 file.write(f'{site}:{encrypted.decode()}\n')
 
     def get_password(self, site):
-        return self.passowrd_dict[site]
+        return self.password_dict[site]
 
 
 def main():
@@ -51,6 +50,44 @@ def main():
         'test_account': 'testaccountPassword'
     }
     pm = PasswordManager()
+
+    print("""Choose an Option
+    1) Create a new key
+    2) Load an existing key
+    3) Create a new password file
+    4) Load existing password file
+    5) Add a new password
+    6) Get a password
+    q) Quit application""")
+
+    done = False
+
+    while not done:
+        choice = input("Pick an Option: ")
+        if choice == '1':
+            path = input('Name the file: ')
+            pm.create_key(path)
+        elif choice == '2':
+            path = input('Enter file name: ')
+            pm.load_key(path)
+        elif choice == '3':
+            path = input('Enter File name: ')
+            pm.create_pass_file(path, password)
+        elif choice == '4':
+            path = input('Enter file name: ')
+            pm.load_pass_file(path)
+        elif choice == '5':
+            site = input('Enter the site: ')
+            password = input('Enter password: ')
+            pm.add_password(site, password)
+        elif choice == '6':
+            site = input('Which site do you want to see: ')
+            print(f'Password for {site} is {pm.get_password(site)} ')
+        elif choice == 'q':
+            done = True
+            print('Have a great day!')
+        else:
+            print('Enter a valid choice please')
 
 
 if __name__ == '__main__':
